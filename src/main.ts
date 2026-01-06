44c237f126d1f1320d921b8b20e812419e072b58
shameer <shameer.myd@gmail.com>
2026-01-06 16:28:13 +0530

    validation pipe globally implemented

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  
+  app.useGlobalPipes( //added for Enable Validation Globally
+    new ValidationPipe({
+      whitelist: true, //removes extra fields
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
   console.log(`port 3000 running....`)
